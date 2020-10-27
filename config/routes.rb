a2a2d31e1e662416614cf8a709b80c658f804f1a
nikikoh <nikikoh5@gmail.com>
2020-10-27 21:27:58 +0900

    deviseのcotrollerとviewの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
-  get 'pages/show'
-  resources :users
-  devise_for :users
+  get 'show' => 'pages/show'
+  get '/new', to: 'users#new'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  resources :users, only: [:show]
+
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_out', to: 'users/sessions#destroy'
+  end
 end
--- db/migrate/20201018115915_add_devise_to_users.rb
+++ db/migrate/20201018115915_add_devise_to_users.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class AddDeviseToUsers < ActiveRecord::Migration[6.0]
-
 end
