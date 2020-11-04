157a5cf64d965084939b7f4d354c7fbfd5ee8c0f
nikikoh <nikikoh5@gmail.com>
2020-11-04 23:41:35 +0900

    login create

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get '/' => 'pages#index'
-  get 'show' => 'pages#show'
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   get 'new', to: 'users#new'
-  get 'edit', to: 'pages#edit'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :users, only: %i[show edit update destroy]
+  resources :tasks
 
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
