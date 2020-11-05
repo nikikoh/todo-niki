5186b33cb65eecfd28f0da47a312a66036dc7ba4
nikikoh <nikikoh5@gmail.com>
2020-11-05 21:25:45 +0900

    create registrationsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    User.find_by(name: super)
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    User.find_by(name: super)
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    User.find_by(name: super)
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    User.find_by(name: super)
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    User.find_by(name: super)
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -46,22 +46,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,5 @@ class UsersController < ApplicationController
   end
 
   def create
-    @users = current_user.articles.build(users_params)
-    if @user.save
-      redirect_to users_path(@user), notice: '保存できたよ'
-    else
-      flash.now[:error] = '保存に失敗しました'
-      render :new
-    end
   end
 end
