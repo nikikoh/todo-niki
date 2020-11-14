243fae94fd623ada77296947e6f307a52967ed0c
nikikoh <nikikoh5@gmail.com>
2020-11-14 21:04:27 +0900

    change

--- db/migrate/20201114095124_create_boards.rb
+++ db/migrate/20201114095124_create_boards.rb
@@ -1,10 +1,10 @@
 class CreateBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :boards do |t|
-      t.string :name
-      t.string :avatar
-      t.text :description
-      t.integer :user_id
+      t.string   :name
+      t.string   :avatar
+      t.text     :description
+      t.integer  :user_id
 
       t.timestamps
     end
