e7bfcac846e00628ffe917efe98f05a50f3f7436
nikikoh <nikikoh5@gmail.com>
2020-11-19 19:47:38 +0900

    Devise　controller&view 工事

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true
+  validates :name,  uniqueness: true
+
+  # 登録時にemailを不要とする
+  def email_required?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+
   has_many :boards, dependent: :destroy
 end
--- db/migrate/20201018115915_add_devise_to_users.rb
+++ db/migrate/20201018115915_add_devise_to_users.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
+
 class AddDeviseToUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      #null: falseにするとnot nullエラー発生
-      t.string :name, null: false
+      t.string :name, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
     end
   end
