0d5c02abe3998ad05e80b9a772ddb2d8c1981741
nikikoh <nikikoh5@gmail.com>
2020-11-19 14:35:32 +0900

    rb　変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :name
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :user, dependent: :destroy
   has_many :boards, dependent: :destroy
-
-  def will_save_change_to_email?
-    false
-  end
 end
