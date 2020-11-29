79b42d9856d49793cf9917cdf6fb6efadcb25f61
nikikoh <nikikoh5@gmail.com>
2020-11-29 20:54:32 +0900

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :current_user
+  helper_method :current_user, :user_signed_in
 
   attr_accessor :name
   protect_from_forgery with: :exception
 
   def current_user
     return unless session[:user_id]
+
     @current_user ||= User.find(session[:user_id])
   end
 
-  def logged_in?
-    !current_user.nil?
+  def user_signed_in
+    !current_user.nil
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
+    devise_parameter_sanitizer.permit :sign_up,        keys: [:name]
     devise_parameter_sanitizer.permit :account_update, keys: [:name]
-    devise_parameter_sanitizer.permit :sign_in, keys: [:name]
+    devise_parameter_sanitizer.permit :sign_in,        keys: [:name]
   end
-
 end
