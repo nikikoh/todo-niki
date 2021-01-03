1958d8b6391b149cff381cd24cd4bb4fde6f2f79
nikikoh <nikikoh5@gmail.com>
2021-01-03 11:29:41 +0900

    chnage

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_131112) do
   create_table "users", force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
