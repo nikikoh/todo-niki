2e88db61b6ac814885388f5a671e7145aab06702
nikikoh <nikikoh5@gmail.com>
2021-01-06 17:34:58 +0900

    コメント　コントローラ完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   root to: 'boards#index'
   devise_for :users
 
   resources :boards do
     resources :tasks
+  end
+
+  resources :tasks do
+    resources :comments
   end
 end
