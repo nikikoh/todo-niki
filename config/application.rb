e1ea8a04f9b089576db45eb4f7120b654d717ac4
nikikoh <nikikoh5@gmail.com>
2021-01-11 00:49:28 +0900

    change

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module TODO
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = %i[en ja]
-    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
