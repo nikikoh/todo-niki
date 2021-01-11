aa03053cbe81c3be3d3452abee8c6aec22abf611
nikikoh <nikikoh5@gmail.com>
2021-01-11 22:39:04 +0900

    change

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module TODO
   class Application < Rails::Application
-
     config.load_defaults 6.0
+
     if Rails.env.development? || Rails.env.test?
       Bundler.require(*Rails.groups)
       Dotenv::Railtie.load
