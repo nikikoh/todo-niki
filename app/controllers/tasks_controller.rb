f5b7bb9c9fe5d59d887779511bb195b61529e068
nikikoh <nikikoh5@gmail.com>
2021-01-15 01:19:46 +0900

    fix: newとeditのフォームにurlを追加し、タスクを作成できるようにしました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,26 +2,26 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show]
 
   def index
-    @tasks = Task.all.order(created_at: :desc)
+    @tasks = Task.all.order
   end
 
   def show
-    @task = Task.find(params[:id])
+    # set_task
     @comments = @task.comments
   end
 
   def new
-    @board = Board.find(params[:board_id])
-    @task  = @board.tasks.build
+    board = Board.find(params[:board_id])
+    @task  = board.tasks.build
     @task.user = current_user
   end
 
   def create
-    @board = Board.find(params[:board_id])
-            @task  = @board.tasks.build(task_params)
+    board = Board.find(params[:board_id])
+            @task  = board.tasks.build(task_params)
             @task.user = current_user
     if @task.save
-      redirect_to board_path(@board), notice: '追加しました'
+      redirect_to board_path(board), notice: '追加しました'
     else
       flash.now[:error] = '追加できませんでした'
       render :new
@@ -29,12 +29,12 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
-    if @task.update(task_params)
+    task = current_user.tasks.find(params[:id])
+    if task.update(task_params)
       redirect_to task_path(task), notice: '更新できました'
     else
       flash.now[:error] = '更新できませんでした'
@@ -43,10 +43,10 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    board = task.board
+    task =  current_user.tasks.find(params[:id])
+            board = task.board
     task.destroy!
-    redirect_to root_path, notice: '削除しました。'
+    redirect_to board_path(board), notice: '削除しました。'
   end
 
   private
